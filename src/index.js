30d3d99d5a3972092916247eeafd2f3261a17066
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2021-02-05 22:45:09 +0100

    Make store.setConfig public

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import Client from './client';
 import Query from './query';
 import DerivedQuery from './derived_query';
 import Mutation from './mutation';
-import { getEntityById } from './store';
+import store from './store';
+
+const getEntityById = store.getEntityById;
+const setStoreConfig = store.setConfig;
 
 export {
   Client,
   Query,
   DerivedQuery,
   Mutation,
-  getEntityById
+  getEntityById,
+  setStoreConfig
 }
