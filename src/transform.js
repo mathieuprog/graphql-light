5251f919e4f5eba3299a4ee4ee373f67bddbad6f
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2021-02-08 11:09:58 +0100

    Add skipNullish option to transform function

--- src/transform.js
+++ src/transform.js
@@ -1,6 +1,14 @@
-export default function transform(entity, resolvers) {
+const defaultTransformOptions = { skipNullish: true };
+
+function isNullish(value) {
+  return value === null || value === undefined;
+}
+
+export default function transform(entity, resolvers, options) {
+  options = Object.assign({}, defaultTransformOptions, options);
+
   for (let [propName, propValue] of Object.entries(entity)) {
-    if (resolvers[propName]) {
+    if (resolvers[propName] && (!options.skipNullish || !isNullish(propValue))) {
       entity[propName] = resolvers[propName](propValue);
     }
   }
--- src/transform.test.js
+++ src/transform.test.js
@@ -1,6 +1,6 @@
 import transform from './transform';
 
-test('create proxy', () => {
+test('transform prop', () => {
   let entity = { name: 'mathieu', age: 37 };
 
   entity = transform(entity, {
@@ -9,11 +9,41 @@ test('create proxy', () => {
 
   expect(entity.name).toBe('MATHIEU');
   expect(entity.age).toBe(37);
+});
 
-  entity = { age: 37 };
+test('transform but prop no present', () => {
+  let entity = { age: 37 };
 
   entity = transform(entity, {
-    name: e => e.toUpperCase()
+    name: n => n.toUpperCase()
+  })
+
+  expect(entity.name).toBe(undefined);
+});
+
+test('transform null', () => {
+  let entity = { name: null };
+
+  entity = transform(entity, {
+    name: _ => 'name'
+  }, { skipNullish: false })
+
+  expect(entity.name).toBe('name');
+});
+
+test('do not transform nullish values', () => {
+  let entity = { name: null };
+
+  entity = transform(entity, {
+    name: n => n.toUpperCase()
+  })
+
+  expect(entity.name).toBe(null);
+
+  entity = { name: undefined };
+
+  entity = transform(entity, {
+    name: n => n.toUpperCase()
   })
 
   expect(entity.name).toBe(undefined);
