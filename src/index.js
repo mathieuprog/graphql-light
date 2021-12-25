6bf63f973f55a58b003ad1a9e7eead520289b361
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2021-12-25 08:40:33 +0100

    Rename files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import Client from './Client';
+import store from './Store';
 import Query from './Query';
 import DerivedQuery from './DerivedQuery';
 import Mutation from './mutation';
-import store from './Store';
 import transform from './transform';
 import FetchStrategy from './fetchStrategies';
 
 export {
-  store,
   Client,
+  store,
   Query,
   DerivedQuery,
   Mutation,
